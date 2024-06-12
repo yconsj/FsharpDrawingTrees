43f5f41e32e6073b801b79f46affa918a3426181
simon <yconsj@gmail.com>
2024-06-12 23:51:24 +0200

    update

--- TreePlotter.fsx
+++ TreePlotter.fsx
@@ -1,11 +1,8 @@
 #r "nuget: Plotly.NET, 4.0.0"
 #r "DrawTrees/Library/drawTrees.dll"
-#r "nuget: FsCheck"
 
 open Plotly.NET
-open System
 open DrawingTrees
-open FsCheck
 open Plotly.NET.LayoutObjects // this namespace contains all object abstractions for layout styling
 
 
@@ -54,13 +51,17 @@ let test () =
 let addPoint p name seq =
     let pSeq =
         Seq.singleton (
+
             Chart.Point(
                 [ p ],
                 MultiText = [ name ],
-                MultiTextPosition = [ StyleParam.TextPosition.TopCenter ],
+                MultiTextPosition = [ StyleParam.TextPosition.MiddleCenter ],
                 ShowLegend = false
+
             )
+            |> Chart.withMarkerStyle (Opacity = 0.0) // Make the marker invisible
         )
+
 
     Seq.append seq pSeq
 
@@ -70,29 +71,69 @@ let addLine (x1, y1) (x2, y2) seq =
 
     Seq.append seq lSeq
 
+let formatter a sep =
 
-let drawTree t =
+    let s = string a
+    let l = (Seq.toList s)
+    let charL = List.splitInto (max (l.Length / sep) 1) l
+    List.map (fun elem -> System.String(elem |> List.toArray)) charL
+
+let drawTreeV1 t =
     let rec drawTreeChildren tree parent seq =
-        let (parentX, parentY) = parent
+        let (parentX, parentY, parentOffset) = parent
 
         match tree with
         | Node((a, v), []) ->
-            let temp = addPoint (parentX + v, parentY - 1.0) a seq
-            addLine (parentX, parentY - 0.1) (parentX + v, parentY - 1.0 + 0.1) temp
+            let formatted = (formatter a 2)
+
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
+
+            let temp =
+                addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+            addLine (parentX, parentY - parentOffset) (parentX + v, parentY - 1.0 + offset) temp
 
 
         | Node((a, v), st) ->
-            let temp = addPoint (parentX + v, parentY - 1.0) a seq
-            let seq = addLine (parentX, parentY - 0.1) (parentX + v, parentY - 1.0 + 0.1) temp
+            let formatted = (formatter a 2)
 
-            List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (parentX + v, parentY - 1.0) seq)) seq st
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
+
+            let temp =
+                addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+            let seq =
+                addLine (parentX, parentY - parentOffset) (parentX + v, parentY - 1.0 + offset) temp
+
+            List.fold
+                (fun acc elem -> Seq.append acc (drawTreeChildren elem (parentX + v, parentY - 1.0, offset) seq))
+                seq
+                st
 
     // Starting at root - shouldnt draw a line
     match t with
-    | Node((a, v), []) -> addPoint (0.0, 0.0) a Seq.empty
+    | Node((a, v), []) ->
+        let formatted = (formatter a 2)
+        addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
     | Node((a, v), st) ->
-        let seq = addPoint (0.0, 0.0) a Seq.empty
-        List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (0.0, 0.0) seq)) seq st
+        let formatted = (formatter a 2)
+
+        let offset =
+            if (float formatted.Length) = 1 then
+                0.1
+            else
+                (float formatted.Length) * 0.075
+
+        let seq = addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
+        List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (0.0, 0.0, offset) seq)) seq st
 
     |> Chart.combine
     |> Chart.withXAxis mirroredXAxis
@@ -100,67 +141,77 @@ let drawTree t =
     |> Chart.show
 
 
+let drawTreeV2 t =
+    let rec drawTreeChildren tree parent seq =
+        let (parentX, parentY) = parent
 
-let tree =
-    Node(
-        "A",
-        [ Node("B", [ Node("C", []); Node("D", []); Node("E", []) ])
-          Node("F", [ Node("G", [ Node("H", []); Node("I", []); Node("J", []) ]) ]) ]
-    )
+        match tree with
+        | Node((a, v), []) ->
+            let formatted = (formatter a 2)
 
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
 
-// Implement generators
-let rec treeGen<'a> (genA: Gen<'a>) (size: int) : Gen<'a Tree> =
-    if size = 0 then
-        gen {
-            let! value = genA
-            return Node(value, [])
-        }
-    else
-        let smallerTreeGen = treeGen genA (size / 2)
+            let seq =
+                addLine (parentX + v, parentY - 1.0 + offset) (parentX + v, parentY + 0.5 - 1.0) seq
 
-        let nodeGen =
-            gen {
-                let! value = genA
-                let! childrenCount = Gen.choose (0, size)
-                let! children = Gen.listOfLength childrenCount smallerTreeGen
-                return Node(value, children)
-            }
-
-        nodeGen
-
-let treeGenSized<'a> (genA: Gen<'a>) : Gen<'a Tree> = Gen.sized (treeGen genA)
-
-// Generate a single tree example
-let generateSampleTree<'a> (genA: Gen<'a>) =
-    let sampleTree = Gen.sample 5 1 (treeGenSized genA) |> List.head
-    sampleTree
-
-List.unzip
-let intTreeGen = treeGenSized Arb.generate<int>
-let sampleTree = generateSampleTree Arb.generate<int>
-
-printf "%A \n" (design sampleTree)
-drawTree (design sampleTree)
+            addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
 
 
+        | Node((a, v), st) ->
+            let formatted = (formatter a 2)
 
-let t =
-    Node(
-        5,
-        [ Node(4, [ Node(-1, []) ])
-          Node(-3, [ Node(-1, [ Node(-4, []) ]); Node(-5, []) ])
-          Node(4, []) ]
-    )
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
 
-printf "%A" (design t)
 
-let wrongTree =
-    Node(
-        (5, 0.0),
-        [ Node((4, -1.25), [ Node((-1, 0.0), []) ])
-          Node((-3, 0.25), [ Node((-1, -0.5), [ Node((-4, 0.0), []) ]); Node((-5, 0.5), []) ])
-          Node((4, 1.25), []) ]
-    )
+            let seq =
+                addLine (parentX + v, parentY - 1.0 + offset) (parentX + v, parentY + 0.5 - 1.0) seq // line up
 
-drawTree wrongTree
+            let seq =
+                addLine (parentX + v, parentY - 1.0 - offset) (parentX + v, parentY - 0.5 - 1.0) seq // Down line
+
+            let (Node((_, vfirst), _)) = st.Head
+            let (Node((_, vlast), _)) = (List.rev st).Head
+
+            let seq =
+                addLine (parentX + v + vfirst, parentY - 1.5) (parentX + v + vlast, parentY - 1.5) seq
+
+
+
+            let seq = addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+            List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (parentX + v, parentY - 1.0) seq)) seq st
+
+    // Starting at root - shouldnt draw a line
+    match t with
+    | Node((a, v), []) -> addPoint (0.0, 0.0) a Seq.empty
+    | Node((a, v), st) ->
+        let formatted = (formatter a 2)
+
+        let offset =
+            if (float formatted.Length) = 1 then
+                0.1
+            else
+                (float formatted.Length) * 0.075
+
+        let seq = addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
+        let seq = addLine (0.0, 0.0 - offset) (0.0, - 0.5) seq
+        let (Node((_, vfirst), _)) = st.Head
+        let (Node((_, vlast), _)) = (List.rev st).Head
+        let seq = addLine (vfirst, -0.5) (vlast, -0.5) seq
+        List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (0.0, 0.0) seq)) seq st
+
+    |> Chart.combine
+    |> Chart.withXAxis mirroredXAxis
+    |> Chart.withYAxis mirroredYAxis
+    |> Chart.show
+
+let renderTree b t =
+    if b then drawTreeV1 t else drawTreeV2 t
