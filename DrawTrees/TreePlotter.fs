fe092179f127a666f47d63330df9eed7040c4ea6
simon <yconsj@gmail.com>
2024-06-12 23:50:45 +0200

    updated lib

---
+++ DrawTrees/TreePlotter.fs
@@ -0,0 +1,198 @@
+namespace DrawTrees
+
+
+module PlotTrees =
+    open Plotly.NET
+    open Plotly.NET.LayoutObjects // this namespace contains all object abstractions for layout styling
+
+    open Trees
+
+
+    let mirroredXAxis =
+        LinearAxis.init (
+            //        Title = Title.init(Text="Mirrored axis"),
+            ShowLine = false,
+            Mirror = StyleParam.Mirror.False,
+            ShowGrid = false
+        //        Ticks = StyleParam.TickOptions.Inside
+        )
+
+    let mirroredYAxis =
+        LinearAxis.init (
+            //        Title = Title.init(Text="Log axis"),
+            //        AxisType = StyleParam.AxisType.Log,
+            ShowLine = false,
+            Mirror = StyleParam.Mirror.False,
+            ShowGrid = false
+        )
+
+    let addPoint p name seq =
+        let pSeq =
+            Seq.singleton (
+
+                Chart.Point(
+                    [ p ],
+                    MultiText = [ name ],
+                    MultiTextPosition = [ StyleParam.TextPosition.MiddleCenter ],
+                    ShowLegend = false
+
+                )
+                |> Chart.withMarkerStyle (Opacity = 0.0) // Make the marker invisible
+            )
+
+
+        Seq.append seq pSeq
+
+    let addLine (x1, y1) (x2, y2) seq =
+        let lSeq =
+            Seq.singleton (Chart.Line([ x1; x2 ], [ y1; y2 ], LineColor = Color.fromString "green", ShowLegend = false))
+
+        Seq.append seq lSeq
+
+    let formatter a sep =
+
+        let s = string a
+        let l = (Seq.toList s)
+        let charL = List.splitInto (max (l.Length / sep) 1) l
+        List.map (fun elem -> System.String(elem |> List.toArray)) charL
+
+    let drawTreeV1 t =
+        let rec drawTreeChildren tree parent seq =
+            let (parentX, parentY, parentOffset) = parent
+
+            match tree with
+            | Node((a, v), []) ->
+                let formatted = (formatter a 2)
+
+                let offset =
+                    if (float formatted.Length) = 1 then
+                        0.1
+                    else
+                        (float formatted.Length) * 0.075
+
+                let temp =
+                    addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+                addLine (parentX, parentY - parentOffset) (parentX + v, parentY - 1.0 + offset) temp
+
+
+            | Node((a, v), st) ->
+                let formatted = (formatter a 2)
+
+                let offset =
+                    if (float formatted.Length) = 1 then
+                        0.1
+                    else
+                        (float formatted.Length) * 0.075
+
+                let temp =
+                    addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+                let seq =
+                    addLine (parentX, parentY - parentOffset) (parentX + v, parentY - 1.0 + offset) temp
+
+                List.fold
+                    (fun acc elem -> Seq.append acc (drawTreeChildren elem (parentX + v, parentY - 1.0, offset) seq))
+                    seq
+                    st
+
+        // Starting at root - shouldnt draw a line
+        match t with
+        | Node((a, v), []) ->
+            let formatted = (formatter a 2)
+            addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
+        | Node((a, v), st) ->
+            let formatted = (formatter a 2)
+
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
+
+            let seq = addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
+            List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (0.0, 0.0, offset) seq)) seq st
+
+        |> Chart.combine
+        |> Chart.withXAxis mirroredXAxis
+        |> Chart.withYAxis mirroredYAxis
+        |> Chart.show
+
+
+    let drawTreeV2 t =
+        let rec drawTreeChildren tree parent seq =
+            let (parentX, parentY) = parent
+
+            match tree with
+            | Node((a, v), []) ->
+                let formatted = (formatter a 2)
+
+                let offset =
+                    if (float formatted.Length) = 1 then
+                        0.1
+                    else
+                        (float formatted.Length) * 0.075
+
+                let seq =
+                    addLine (parentX + v, parentY - 1.0 + offset) (parentX + v, parentY + 0.5 - 1.0) seq
+
+                addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+
+            | Node((a, v), st) ->
+                let formatted = (formatter a 2)
+
+                let offset =
+                    if (float formatted.Length) = 1 then
+                        0.1
+                    else
+                        (float formatted.Length) * 0.075
+
+
+                let seq =
+                    addLine (parentX + v, parentY - 1.0 + offset) (parentX + v, parentY + 0.5 - 1.0) seq // line up
+
+                let seq =
+                    addLine (parentX + v, parentY - 1.0 - offset) (parentX + v, parentY - 0.5 - 1.0) seq // Down line
+
+                let (Node((_, vfirst), _)) = st.Head
+                let (Node((_, vlast), _)) = (List.rev st).Head
+
+                let seq =
+                    addLine (parentX + v + vfirst, parentY - 1.5) (parentX + v + vlast, parentY - 1.5) seq
+
+
+
+                let seq = addPoint (parentX + v, parentY - 1.0) (String.concat "<br>" formatted) seq
+
+                List.fold
+                    (fun acc elem -> Seq.append acc (drawTreeChildren elem (parentX + v, parentY - 1.0) seq))
+                    seq
+                    st
+
+        // Starting at root - shouldnt draw a line
+        match t with
+        | Node((a, v), []) -> addPoint (0.0, 0.0) a Seq.empty
+        | Node((a, v), st) ->
+            let formatted = (formatter a 2)
+
+            let offset =
+                if (float formatted.Length) = 1 then
+                    0.1
+                else
+                    (float formatted.Length) * 0.075
+
+            let seq = addPoint (0.0, 0.0) (String.concat "<br>" formatted) Seq.empty
+            let seq = addLine (0.0, 0.0 - offset) (0.0, - 0.5) seq
+            let (Node((_, vfirst), _)) = st.Head
+            let (Node((_, vlast), _)) = (List.rev st).Head
+            let seq = addLine (vfirst, -0.5) (vlast, -0.5) seq
+            List.fold (fun acc elem -> Seq.append acc (drawTreeChildren elem (0.0, 0.0) seq)) seq st
+
+        |> Chart.combine
+        |> Chart.withXAxis mirroredXAxis
+        |> Chart.withYAxis mirroredYAxis
+        |> Chart.show
+
+    let renderTree b t =
+        if b then drawTreeV1 t else drawTreeV2 t
